ba5f1f73a4b1620a22ff8d06553223d8b6c01a86
sycLin <b01902044@ntu.edu.tw>
2018-01-18 23:23:50 +0800

    + add testing code for General Model

---
+++ GeneralModelTest.py
@@ -0,0 +1,15 @@
+import numpy as np
+from keras.models import load_model
+
+model_path = "data/generalData/general_Model.hdf5"
+
+test_data_images_path = "test_data_images.nparray"
+test_data_targets_path = "test_data_targets.nparray"
+
+model = load_model(model_path)
+
+test_images = np.load(test_data_images_path)
+test_targets = np.load(test_data_targets_path)
+scores = model.evaluate(test_images, test_targets, verbose=0)
+
+print(scores)
